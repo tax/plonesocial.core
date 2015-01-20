bc9b58fb00ac06558b74c7dbd4f287ffd5ceed9e
Alessandro Pisa <alessandro.pisa@redturtle.it>
2015-01-20 11:22:56 +0100

    Setup testing machinery

--- setup.py
+++ setup.py
@@ -11,42 +11,51 @@ long_description = (
     open('CONTRIBUTORS.rst').read()
     + '\n' +
     open('CHANGES.rst').read()
-    + '\n')
+    + '\n'
+)
 
-setup(name='plonesocial.core',
-      version=version,
-      description="Plonesocial core package",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Environment :: Web Environment',
-          'Framework :: Plone :: 4.2',
-          'Framework :: Plone :: 4.3',
-          'Framework :: Plone',
-          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-      ],
-      keywords='plone socbiz social microblog activity stream',
-      author='Bogdan Girman',
-      author_email='bogdan.girman@gmail.com',
-      url='http://github.com/cosent/plonesocial.core',
-      license='gpl',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['plonesocial'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
+setup(
+    name='plonesocial.core',
+    version=version,
+    description="Plonesocial core package",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Plone :: 4.2',
+        'Framework :: Plone :: 4.3',
+        'Framework :: Plone',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='plone socbiz social microblog activity stream',
+    author='Bogdan Girman',
+    author_email='bogdan.girman@gmail.com',
+    url='http://github.com/cosent/plonesocial.core',
+    license='gpl',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['plonesocial'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+        'plone.api',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            'unittest2',
+        ],
+    },
+    entry_points="""
       # -*- Entry points: -*-
-      """,
-      )
+    """,
+)
