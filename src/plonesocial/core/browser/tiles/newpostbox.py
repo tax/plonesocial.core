48d6d290fe4567f45650feaa4b14159190fe95f9
Alessandro Pisa <alessandro.pisa@redturtle.it>
2015-02-03 17:42:34 +0100

    Do not call the stream view

--- src/plonesocial/core/browser/tiles/newpostbox.py
+++ src/plonesocial/core/browser/tiles/newpostbox.py
@@ -1,23 +1,12 @@
 # -*- coding: utf-8 -*-
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone import api
 from plone.tiles import Tile
 from plonesocial.core.browser.stream import StreamBase
+from plonesocial.core.integration import PLONESOCIAL
+from zope.component import getMultiAdapter
 
 
 class NewPostBoxTile(Tile, StreamBase):
 
     index = ViewPageTemplateFile('templates/new-post-box-tile.pt')
 
-    def render(self):
-        return self.index()
-
-    def __call__(self):
-        if self.request.form:
-            view = api.content.get_view(
-                'stream',
-                self.context,
-                self.request,
-            )
-            view()
-        return self.render()
