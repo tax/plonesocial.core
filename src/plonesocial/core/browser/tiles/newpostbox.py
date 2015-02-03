b614c177a56bd153c6f7fed3abceba67f874902d
Alessandro Pisa <alessandro.pisa@redturtle.it>
2015-02-03 17:56:30 +0100

    flake8

--- src/plonesocial/core/browser/tiles/newpostbox.py
+++ src/plonesocial/core/browser/tiles/newpostbox.py
@@ -2,8 +2,6 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.tiles import Tile
 from plonesocial.core.browser.stream import StreamBase
-from plonesocial.core.integration import PLONESOCIAL
-from zope.component import getMultiAdapter
 
 
 class NewPostBoxTile(Tile, StreamBase):
